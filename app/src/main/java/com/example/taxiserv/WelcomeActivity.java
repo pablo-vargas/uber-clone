8ca8f7a2813797051e1c9f90f31a64e8bd41b518
CErwing <choqueriveErwing@gmail.com>
2020-08-05 15:56:08 -0400

    navigation drawer agregado

--- app/src/main/java/com/example/taxiserv/WelcomeActivity.java
+++ app/src/main/java/com/example/taxiserv/WelcomeActivity.java
@@ -1,39 +1,77 @@
 package com.example.taxiserv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.taxiserv.models.Client;
 import com.example.taxiserv.utils.FirebaseAuthenticationAPI;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
 
-public class WelcomeActivity extends AppCompatActivity {
-    Button btnExit;
+public class WelcomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    CircleImageView cvProfile;
+    TextView tvUsername;
+    TextView tvEmail;
+    
     FirebaseAuthenticationAPI mAuth;
+    Client currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        
-        btnExit = (Button)findViewById(R.id.btnExit);
-        mAuth = new FirebaseAuthenticationAPI();
-        
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                exit();
-            }
-        });
 
-        Client client = mAuth.getAuthClient();
-        Log.e("name", client.getUsername());
-        Log.e("email", client.getEmail());
-        Log.e("photo", client.getPhotoUrl());
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView)findViewById(R.id.navView);
+        View hView = navigationView.getHeaderView(0);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        tvUsername = (TextView) hView.findViewById(R.id.tvUsernameDrawer);
+        tvEmail = (TextView) hView.findViewById(R.id.tvEmailDrawer);
+        cvProfile = (CircleImageView) hView.findViewById(R.id.cvProfileDrawer);
+
+        mAuth = new FirebaseAuthenticationAPI();
+
+        setupToolbar();
+        setupDatas();
+        setFragmentByDefault();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setupDatas() {
+        Picasso.get()
+                .load(getCurrentClient().getPhotoUrl())
+                .placeholder(R.drawable.ic_baseline_person)
+                .error(R.drawable.ic_baseline_person)
+                .into(cvProfile);
+        tvUsername.setText(getCurrentClient().getUsername());
+        tvEmail.setText(getCurrentClient().getEmail());
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setFragmentByDefault() {
+        MenuItem item = navigationView.getMenu().getItem(0);
+        item.setChecked(true);
+        getSupportActionBar().setTitle(item.getTitle());
     }
 
     private void exit() {
@@ -41,5 +79,47 @@ public class WelcomeActivity extends AppCompatActivity {
         Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private Client getCurrentClient() {
+        if(currentUser == null) {
+            currentUser = mAuth.getAuthClient();
+        }
+        return currentUser;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_start:
+                Toast.makeText(WelcomeActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_profile:
+                Toast.makeText(WelcomeActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_history:
+                Toast.makeText(WelcomeActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_termsUse:
+                Toast.makeText(WelcomeActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_sigOut:
+                exit();
+                break;
+        }
+        item.setChecked(true);
+        drawerLayout.closeDrawers();
+        getSupportActionBar().setTitle(item.getTitle());
+        return true;
     }
 }
